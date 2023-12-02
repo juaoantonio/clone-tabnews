765f4fb8729376c51976866e67312bcb6f74d8b4
João Antônio Barbosa <joaobarbosadev@gmail.com>
2023-12-02 03:24:56 -0300

    feat: add updated_at, max_connections, opened_connections and db version in status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,25 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as SUM");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const version = (
+    await database.query("SELECT split_part(version(), ' ', 2);")
+  ).rows[0].split_part;
+
+  const maxConnections = (await database.query("SHOW max_connections;")).rows[0]
+    .max_connections;
+
+  const openedConnections = (
+    await database.query("SELECT * FROM pg_stat_activity;")
+  ).rows.length;
 
   res.status(200).json({
-    chave: "são acima da média",
+    updated_at: updatedAt,
+    dependencies: {
+      version,
+      max_connections: +maxConnections,
+      opened_connections: openedConnections,
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const {
+    updated_at,
+    dependencies: { version, max_connections, opened_connections },
+  } = responseBody;
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(updated_at).toEqual(parsedUpdatedAt);
+
+  expect(version).toBeTruthy();
+
+  expect(max_connections).toBeGreaterThan(0);
+
+  expect(opened_connections).toBeLessThanOrEqual(max_connections);
 });
