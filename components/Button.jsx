e5444bb3c3bbb42fa71959b3f1bf88e8624acfc3
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-07-20 02:50:32 -0300

    chore: apply consistent formatting across codebase

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,10 +1,16 @@
-export default function Button({children, onClick}) {
-
-    return (<button style={{
-        border: 'none',
-        padding: '.8rem 2rem',
-        fontSize: '1.5rem',
-        cursor: 'pointer',
-        borderRadius: '5px'
-    }} onClick={onClick}>{children}</button>)
+export default function Button({ children, onClick }) {
+  return (
+    <button
+      style={{
+        border: "none",
+        padding: ".8rem 2rem",
+        fontSize: "1.5rem",
+        cursor: "pointer",
+        borderRadius: "5px",
+      }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 }
