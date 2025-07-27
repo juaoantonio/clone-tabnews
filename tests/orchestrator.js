f50189ec79fb59b57708204b9572a708aec4b7db
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-07-26 23:36:53 -0300

    chore: refactor module exports to use named constants and suppress unused variable lint warning

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,4 +1,5 @@
 import retry from "async-retry";
+
 async function waitForAllServices() {
   await waitForWebServer();
 
@@ -17,6 +18,8 @@ async function waitForAllServices() {
   }
 }
 
-export default {
+const orchestrator = {
   waitForAllServices,
 };
+
+export default orchestrator;
