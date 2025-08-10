5ee2980ebbd66b41323e28eaf64840f38dc1ca1a
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-08-10 16:50:23 -0300

    refactor: move migrations related functions to `migrator` model

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,11 @@
 import { Client } from "pg";
-import { resolve } from "node:path";
-import migrationRunner from "node-pg-migrate";
 import { ServiceUnavailableError } from "./errors";
 
 async function query(queryObject) {
   let client;
   try {
     client = await getNewClient();
-    const result = await client.query(queryObject);
+    const result = client.query(queryObject);
     return result;
   } catch (error) {
     const serviceUnavailableError = new ServiceUnavailableError({
@@ -36,41 +34,9 @@ async function getNewClient() {
   return client;
 }
 
-function getDefaultMigrationOptions() {
-  return {
-    dryRun: true,
-    dir: resolve("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-}
-
-async function migrationRun(options) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    return await migrationRunner({
-      ...options,
-      dbClient,
-    });
-  } catch (error) {
-    const serviceUnavailableError = new ServiceUnavailableError({
-      message: "Não foi possível conectar ao banco de dados.",
-      cause: error,
-    });
-    console.error(serviceUnavailableError);
-    throw serviceUnavailableError;
-  } finally {
-    dbClient?.end();
-  }
-}
-
 const database = {
   getNewClient,
   query,
-  getDefaultMigrationOptions,
-  migrationRun,
 };
 
 export default database;
