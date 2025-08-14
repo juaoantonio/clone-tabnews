95fc6ffa2c57a2e70aa5e85c46f2b9d66686c49b
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-08-14 11:38:31 -0300

    feat: implement find by username endpoint and logic

---
+++ pages/api/v1/users/[username]/index.js
@@ -0,0 +1,14 @@
+import { createRouter } from "next-connect";
+import controller from "infra/controller";
+import user from "@models/user";
+
+const router = createRouter();
+router.get(getHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function getHandler(req, res) {
+  const username = req.query.username;
+  const result = await user.findOneByUsername(username);
+  return res.status(200).json(result);
+}
